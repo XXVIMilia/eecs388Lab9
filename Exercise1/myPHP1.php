d6de02f7ae96b4f7c9b21d817e72cd900335fa98
Chauncey Hester <c112h406@ku.edu>
2021-05-05 05:57:07 -0500

    Test two of mult table

--- Exercise1/myPHP1.php
+++ Exercise1/myPHP1.php
@@ -2,7 +2,11 @@
 
 function printer(){
     for($a = 0; $a <= 100; $a++){
-        echo "$a <br>";
+        echo "$a ";
+        for($b = 1; $b <=100; $b++){
+            echo "$b ";
+        }
+        echo "<br>";
     }
 }
 
