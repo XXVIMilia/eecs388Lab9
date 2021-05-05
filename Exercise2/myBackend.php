ad622f4a469239ca377623e434558a9faf779929
Chauncey Hester <c112h406@ku.edu>
2021-05-05 13:14:13 -0500

    Excercise2 test 3

--- Exercise2/myBackend.php
+++ Exercise2/myBackend.php
@@ -7,38 +7,38 @@ $q4 = $_POST["q4"];
 $q5 = $_POST["q5"];
 
 function getResults(){
-    //$vals = array[$q1,$q2,$q3,$q4,$q5];
+    $vals = array($q1,$q2,$q3,$q4,$q5);
     $correct = 0;
-    // for($a = 0; $a < 5; $a++){
-    //     if($vals[$a] == $answers[$a]){
-    //         $correct++;
-    //     }
-    // }
+    for($a = 0; $a < 5; $a++){
+        if($vals[$a] == $answers[$a]){
+            $correct++;
+        }
+    }
     return($correct);
 }
 
 $answers = array("Sam","The Boston Molasses Disaster", "19", "Lord Kelvin", "A");
+echo "<h1>"
+echo "Question 1: Which name has the least amount of letters?<br>";
+echo "You answered" . $q1 .  "<br>";
+echo "Corret answer" . $answers[0] .  "<br><br>";
 
-// echo "Question 1: Which name has the least amount of letters?";
-// echo "You answered" . $q1 .  "<br>";
-// echo "Corret answer" . $answers[0] .  "<br><br>";
+echo "Question 2: Which significant event occured in 1918?<br>";
+echo "You answered" . $q2 .  "<br>";
+echo "Corret answer" . $answers[1] .  "<br><br>";
 
-// echo "Question 2: Which significant event occured in 1918?";
-// echo "You answered" . $q2 .  "<br>";
-// echo "Corret answer" . $answers[1] .  "<br><br>";
+echo "Question 3: My twin lives at the reverse of my house number. The difference between our house numbers ends in two. What are the lowest possible numbers of our house?<br>";
+echo "You answered" . $q3 .  "<br>";
+echo "Corret answer" . $answers[2] .  "<br><br>";
 
-// echo "Question 3: My twin lives at the reverse of my house number. The difference between our house numbers ends in two. What are the lowest possible numbers of our house?";
-// echo "You answered" . $q3 .  "<br>";
-// echo "Corret answer" . $answers[2] .  "<br><br>";
+echo "Question 4: Which Scientist had a noteworthy beard?<br>";
+echo "You answered" . $q4 .  "<br>";
+echo "Corret answer" . $answers[3] .  "<br><br>";
 
-// echo "Question 4: Which Scientist had a noteworthy beard?";
-// echo "You answered" . $q4 .  "<br>";
-// echo "Corret answer" . $answers[3] .  "<br><br>";
-
-echo "Question 5: Lowest key on an 88 key piano?";
-echo "You answered" . $q5 .  "<br>";
+echo "Question 5: Lowest key on an 88 key piano?<br>";
+echo "You answered: " . $q5 .  "<br>";
 echo "Corret answer: " . $answers[4] .  "<br><br>";
 
 echo getResults();
-
+echo "</h1>"
 ?>
