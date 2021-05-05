ba004a259b563c33de58f681a1e36add512a7672
Chauncey Hester <c112h406@ku.edu>
2021-05-05 06:17:27 -0500

    Test four of mult table

--- Exercise1/myPHP1.php
+++ Exercise1/myPHP1.php
@@ -1,9 +1,22 @@
 <?php
 
-function printer(){
-    echo "Fuck"
+function rowPrint($mult, $tag){
+    echo "<tr>";
+    for($a = 0; $a <=100; $a++){
+        echo "<tag>" .. $a .. "</$tag>";
+    }
+    echo "</tr>";
+
 }
 
+function printer(){
+    rowPrint(1,"th");
+    
+}
+
+
+echo "<table>";
 printer();
+echo "</table>"
 
 ?>
