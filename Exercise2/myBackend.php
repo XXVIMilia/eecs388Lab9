b0926fbd9a722716b10d00461615e87dd20a5e3c
Chauncey Hester <c112h406@ku.edu>
2021-05-05 14:24:34 -0500

    Excercise2 test 3

--- Exercise2/myBackend.php
+++ Exercise2/myBackend.php
@@ -24,27 +24,26 @@ function getResults($answers){
     return($correct);
 }
 
-
 echo "<span style='font-size:50px'>";
 echo "Question 1: Which name has the least amount of letters?<br>";
-echo "You answered: " . $q1 .  "<br>";
-echo "Correct answer: " . $answers[0] .  "<br><br>";
+echo "&nbsp &nbsp&nbsp &nbspYou answered: " . $q1 .  "<br>";
+echo "&nbsp &nbsp&nbsp &nbspCorrect answer: " . $answers[0] .  "<br><br>";
 
 echo "Question 2: Which significant event occured in 1918?<br>";
-echo "You answered: " . $q2 .  "<br>";
-echo "Correct answer: " . $answers[1] .  "<br><br>";
+echo "&nbsp &nbsp&nbsp &nbspYou answered: " . $q2 .  "<br>";
+echo "&nbsp &nbsp&nbsp &nbspCorrect answer: " . $answers[1] .  "<br><br>";
 
 echo "Question 3: My twin lives at the reverse of my house number. The difference between our house numbers ends in two. What are the lowest possible numbers of our house?<br>";
-echo "You answered: " . $q3 .  "<br>";
-echo "Correct answer: " . $answers[2] .  "<br><br>";
+echo "&nbsp &nbsp&nbsp &nbspYou answered: " . $q3 .  "<br>";
+echo "&nbsp &nbsp&nbsp &nbspCorrect answer: " . $answers[2] .  "<br><br>";
 
 echo "Question 4: Which Scientist had a noteworthy beard?<br>";
-echo "You answered: " . $q4 .  "<br>";
-echo "Correct answer: " . $answers[3] .  "<br><br>";
+echo "&nbsp &nbsp&nbsp &nbspYou answered: " . $q4 .  "<br>";
+echo "&nbsp &nbsp&nbsp &nbspCorrect answer: " . $answers[3] .  "<br><br>";
 
 echo "Question 5: Lowest key on an 88 key piano?<br>";
-echo "You answered: " . $q5 .  "<br>";
-echo "Correct answer: " . $answers[4] .  "<br><br>";
+echo "&nbsp &nbsp&nbsp &nbspYou answered: " . $q5 .  "<br>";
+echo "&nbsp &nbsp&nbsp &nbspCorrect answer: " . $answers[4] .  "<br><br>";
 
 $num = getResults($answers);
 $percent = ($num/5)*100;
