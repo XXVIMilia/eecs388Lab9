58bba097ae97d790f81d9a07cd5aa6be8e7586ae
Chauncey Hester <c112h406@ku.edu>
2021-05-05 13:38:06 -0500

    Excercise2 test 3

--- Exercise2/myBackend.php
+++ Exercise2/myBackend.php
@@ -18,7 +18,7 @@ function getResults(){
 }
 
 $answers = array("Sam","The Boston Molasses Disaster", "19", "Lord Kelvin", "A");
-echo "<span style='font-size:20px'>";
+echo "<span style='font-size:50px'>";
 echo "Question 1: Which name has the least amount of letters?<br>";
 echo "You answered: " . $q1 .  "<br>";
 echo "Correct answer: " . $answers[0] .  "<br><br>";
