a03b075270311e295409bd07a5542ef6722a9f27
Chauncey Hester <c112h406@ku.edu>
2021-05-05 13:04:22 -0500

    Excercise2 test 3

--- Exercise2/myBackend.php
+++ Exercise2/myBackend.php
@@ -9,11 +9,11 @@ $q5 = $_POST["q5"];
 function getResults(){
     $vals = array[$q1,$q2,$q3,$q4,$q5];
     $correct = 0;
-    for($a = 0; $a < 5; $a++){
-        if($vals[$a] == $answers[$a]){
-            $correct++;
-        }
-    }
+    // for($a = 0; $a < 5; $a++){
+    //     if($vals[$a] == $answers[$a]){
+    //         $correct++;
+    //     }
+    // }
     return($correct);
 }
 
