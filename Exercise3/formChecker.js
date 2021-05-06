7d4c5b27cec1316f00c0d1f9b10950f5a3b9b855
Chauncey Hester <c112h406@ku.edu>
2021-05-06 09:04:33 -0500

    Excercise3 validate form version1

--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,8 +1,8 @@
 function load(){
-    document.getElementById("ch").value = ""
-    document.getElementById("e").value = "" 
-    document.getElementById("c").value = ""
-    document.getElementById("h").value = ""
+    document.getElementById("ch").value = "0"
+    document.getElementById("e").value = "0" 
+    document.getElementById("c").value = "0"
+    document.getElementById("h").value = "0"
     document.getElementById("user").value = ""
     document.getElementById("pass").value = ""
     var bttn = document.getElementsByName("q3")
@@ -11,5 +11,33 @@ function load(){
 }
 
 function validateForm(){
-    return true;
+    var list = [document.getElementById("user").value,
+    document.getElementById("pass").value,
+    document.getElementById("ch").value,
+    document.getElementById("e").value,
+    document.getElementById("c").value,
+    document.getElementById("h").value,]
+    var bttn = document.getElementsByName("q3").value
+
+    for(var i = 0; i < list.length; i++){
+        if(list[i] == ""){
+            alert("Missing values")
+            return false
+        }
+    }
+    var ship = False
+    for(var i=0;i<bttn.length;i++)
+        if(bttn[i].checked == true){
+            ship = true
+        }
+
+    if(ship){
+        return true
+    }
+    else{
+        alert("Select a shipping")
+        return false
+    }
+
+
 }
